b6618c7ef59f3bf217fe04ec3929894fad2fac9b
Roni Rahmadi <90230014+ronirahmadi@users.noreply.github.com>
2024-07-13 18:50:52 +0700

    FE back

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = "tbl_users";
-
     /**
      * The attributes that are mass assignable.
      *

35fe24f3b93f8b4cad878e1a34e688494002ef81
gampang123 <gampangrozaki@gmail.com>
2024-07-13 15:51:19 +0700

    FE3

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -16,32 +16,33 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-        <link href="{{ asset('assets/vendors/animate/animate.min.css') }}" />
-        <link href="{{ asset('assets/vendors/animate/custom-animate.css') }}" />
-        <link href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css') }}" />
-        <link href="{{ asset('assets/vendors/bootstrap-select/css/bootstrap-select.min.css') }}" />
-        <link href="{{ asset('assets/vendors/bxslider/jquery.bxslider.css') }}" />
-        <link href="{{ asset('assets/vendors/fontawesome/css/all.min.css') }}" />
-        <link href="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css') }}" />
-        <link href="{{ asset('assets/vendors/jquery-ui/jquery-ui.css') }}" />
-        <link href="{{ asset('assets/vendors/nice-select/nice-select.css') }}" />
-        <link href="{{ asset('assets/vendors/nouislider/nouislider.min.css') }}" />
-        <link href="{{ asset('assets/vendors/nouislider/nouislider.pips.css') }}" />
-        <link href="{{ asset('assets/vendors/odometer/odometer.min.css') }}" />
-        <link href="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.css') }}" />
-        <link href="{{ asset('assets/vendors/owl-carousel/owl.theme.default.min.css') }}" />
-        <link href="{{ asset('assets/vendors/swiper/swiper.min.css') }}" />
-        <link href="{{ asset('assets/vendors/timepicker/timePicker.css') }}" />
-        <link href="{{ asset('assets/vendors/tiny-slider/tiny-slider.min.css') }}" />
-        <link href="{{ asset('assets/vendors/vegas/vegas.min.css') }}" />
-        <link href="{{ asset('assets/vendors/thm-icons/style.css') }}">
-        <link href="{{ asset('assets/vendors/slick-slider/slick.css') }}">
-        <link href="{{ asset('assets/vendors/language-switcher/polyglot-language-switcher.css') }}">
-        <link href="{{ asset('assets/vendors/reey-font/stylesheet.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendors/animate/animate.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/animate/custom-animate.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap/css/bootstrap.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-select/css/bootstrap-select.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/bxslider/jquery.bxslider.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/fontawesome/css/all.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/jquery-ui/jquery-ui.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/nice-select/nice-select.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/nouislider/nouislider.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/nouislider/nouislider.pips.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/odometer/odometer.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.carousel.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel/owl.theme.default.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/swiper/swiper.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/timepicker/timePicker.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/tiny-slider/tiny-slider.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/vegas/vegas.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendors/thm-icons/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendors/slick-slider/slick.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendors/language-switcher/polyglot-language-switcher.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendors/reey-font/stylesheet.css') }}">
 
         <!-- template styles -->
-        <link href="{{ asset('assets/css/style.css') }}" />
-        <link href="{{ asset('assets/css/responsive.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
+
 
 </head>
 

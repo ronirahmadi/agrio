2db92c2e2fba50c590ba8b8b2da5ed8d20e0ae12
gampang123 <gampangrozaki@gmail.com>
2024-07-13 21:26:26 +0700

    ubah load

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -55,7 +55,9 @@
             <div class="preloader-close">x</div>
             <div id="handle-preloader" class="handle-preloader">
                 <div class="animation-preloader">
-                    <div class="spinner"></div>
+                    <div class="spinner">
+                        <img src="{{ asset('assets/img/agrio - load.png') }}" alt="">
+                    </div>
                     <div class="txt-loading">
                         <span data-text-preloader="A" class="letters-loading">
                             A
